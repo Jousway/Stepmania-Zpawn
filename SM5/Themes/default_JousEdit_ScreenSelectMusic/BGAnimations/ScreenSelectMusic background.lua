586d9b23734d6ed3f74d4b45c63c1686000e23a5
Alexander Griffin <alexander_goudjeb@hotmail.com>
2013-04-11 16:23:58 +0200

    Updating Song Background on ScreenSelectMusic

--- SM5/Themes/default_JousEdit_ScreenSelectMusic/BGAnimations/ScreenSelectMusic background.lua
+++ SM5/Themes/default_JousEdit_ScreenSelectMusic/BGAnimations/ScreenSelectMusic background.lua
@@ -1,21 +1,40 @@
+--V3
 return Def.ActorFrame {
-	Def.Sprite {
-	CurrentSongChangedMessageCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;finishtweening;sleep,0.32;queuecommand,"ModifySongBackground");
-	ModifySongBackgroundCommand=function(self)
 	
-		if GAMESTATE:GetCurrentSong() then
-			if GAMESTATE:GetCurrentSong():GetBackgroundPath() then
-				self:visible(true);
-				self:LoadBackground(GAMESTATE:GetCurrentSong():GetBackgroundPath());
-				self:stretchto(0,0,SCREEN_WIDTH,SCREEN_HEIGHT);
+	--No Background Image
+	Def.ActorFrame {
+		LoadActor(THEME:GetPathB("ScreenWithMenuElements","background/Default"));
+		LoadFont("Common Normal") .. {
+			OnCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;zoom,4;diffuse,1,1,1,1;strokecolor,Color("Black"));
+			CurrentSongChangedMessageCommand=function(self)
+				if GAMESTATE:GetCurrentSong() then
+					if GAMESTATE:GetCurrentSong():GetBackgroundPath() then
+						self:settext("");
+					else
+						self:settext("No Background");
+					end;
+				else
+					self:settext("");
+				end;
+			end;
+		};
+	};
+	
+	--Load Song Background
+	Def.Sprite {
+		CurrentSongChangedMessageCommand=cmd(x,SCREEN_CENTER_X;y,SCREEN_CENTER_Y;finishtweening;sleep,0.32;queuecommand,"ModifySongBackground");
+		ModifySongBackgroundCommand=function(self)
+			if GAMESTATE:GetCurrentSong() then
+				if GAMESTATE:GetCurrentSong():GetBackgroundPath() then
+					self:visible(true);
+					self:LoadBackground(GAMESTATE:GetCurrentSong():GetBackgroundPath());
+					self:stretchto(0,0,SCREEN_WIDTH,SCREEN_HEIGHT);
+				else
+					self:visible(false);
+				end;
 			else
 				self:visible(false);
 			end;
-		else
-			self:visible(false);
-		end;
-	end;
-	
+		end;	
 	};
-
 };
